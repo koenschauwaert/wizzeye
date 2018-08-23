e7fab08547c4281964a45ddae8917e00f5a74b69
Koen Schauwaert <koen_schauwaert@outlook.com>
2018-08-23 16:06:34 +0200

    app: Validate room name

--- app/src/main/java/app/wizzeye/app/fragments/RoomSelectionFragment.java
+++ app/src/main/java/app/wizzeye/app/fragments/RoomSelectionFragment.java
@@ -25,25 +25,80 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.StringRes;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 import app.wizzeye.app.R;
 import app.wizzeye.app.SettingsActivity;
 import app.wizzeye.app.call.CallService;
 
-public class RoomSelectionFragment extends BaseFragment {
+public class RoomSelectionFragment extends BaseFragment implements TextWatcher {
+
+    private static final Pattern ROOM_CHARACTERS = Pattern.compile("^[-_a-z0-9]*$");
 
     private EditText mRoom;
+    private TextInputLayout mRoomLayout;
+    private Button mJoin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_room_selection, container, false);
         mRoom = view.findViewById(R.id.room);
-        view.findViewById(R.id.join).setOnClickListener(v -> joinRoom());
+        mRoom.addTextChangedListener(this);
+        mRoomLayout = view.findViewById(R.id.room_layout);
+        mJoin = view.findViewById(R.id.join);
+        mJoin.setOnClickListener(v -> joinRoom());
         return view;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String name = s.toString();
+
+        // Clean up room name
+        name = name.toLowerCase();
+        name = name.replaceAll("\\s", "-");
+        if (!name.equals(s.toString())) {
+            s.replace(0, s.length(), name);
+            return;
+        }
+
+        // Validate room name
+        @StringRes int error = 0;
+        if (!name.isEmpty()) {
+            if (!ROOM_CHARACTERS.matcher(name).matches()) {
+                error = R.string.roomselection_error_invalid;
+            } else if (name.length() < 5) {
+                error = R.string.roomselection_error_short;
+            } else if (name.length() > 64) {
+                error = R.string.roomselection_error_long;
+            }
+        }
+
+        // Apply
+        if (error != 0) {
+            mRoomLayout.setError(getString(error));
+        } else {
+            mRoomLayout.setErrorEnabled(false);
+        }
+        mJoin.setEnabled(error == 0 && !name.isEmpty());
     }
 
     private void joinRoom() {
