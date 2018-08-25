de97c8693d75781ea6fb26bce845f9d30aa1f5f9
Vianney le Clément de Saint-Marcq <code@quartic.eu>
2018-08-25 19:26:45 +0200

    web: Propose random room name

--- server/webroot/s/js/index.js
+++ server/webroot/s/js/index.js
@@ -22,7 +22,17 @@
 
 $("#nojs").hide();
 
+let colors = ["amber", "aquamarine", "azure", "beige", "black", "blue", "bronze", "brown", "copper", "cyan", "emerald", "gold", "gray", "green", "indigo", "ivory", "magenta", "maroon", "orange", "pink", "purple", "red", "rose", "ruby", "silver", "taupe", "teal", "turquoise", "violet", "white", "yellow"];
+let animals = ["cat", "cattle", "dog", "donkey", "goat", "horse", "pig", "rabbit", "aardvark", "aardwolf", "albatross", "alligator", "alpaca", "anaconda", "angelfish", "ant", "anteater", "antelope", "ape", "armadillo", "baboon", "badger", "bandicoot", "barracuda", "basilisk", "bat", "bear", "beaver", "bedbug", "bee", "beetle", "bird", "bison", "blackbird", "boa", "boar", "bobcat", "bonobo", "bug", "butterfly", "buzzard", "camel", "buffalo", "carp", "cat", "catshark", "caterpillar", "catfish", "cattle", "centipede", "chameleon", "cheetah", "chicken", "chimpanzee", "chinchilla", "chipmunk", "clam", "clownfish", "cobra", "cockroach", "cod", "condor", "constrictor", "coral", "cougar", "cow", "coyote", "crab", "crane", "crayfish", "crocodile", "deer", "dog", "dolphin", "donkey", "dormouse", "dragonfly", "dragon", "duck", "eagle", "earthworm", "earwig", "eel", "elephant", "emu", "falcon", "ferret", "firefly", "fish", "flamingo", "flea", "fly", "fox", "frog", "gazelle", "gecko", "giraffe", "goat", "goldfish", "goose", "gorilla", "grasshopper", "grouse", "guppy", "hamster", "hawk", "hedgehog", "hippo", "hookworm", "hornet", "horse", "hoverfly", "hummingbird", "hyena", "impala", "jackal", "jaguar", "jellyfish", "kangaroo", "kingfisher", "kiwi", "koala", "koi", "krill", "ladybug", "leech", "lemming", "leopard", "lion", "lizard", "llama", "lobster", "lungfish", "lynx", "macaw", "mackerel", "mammal", "manatee", "mandrill", "marlin", "marmot", "marsupial", "meerkat", "mite", "mockingbird", "mole", "mongoose", "monkey", "moose", "mosquito", "moth", "mouse", "mule", "nightingale", "octopus", "orangutan", "orca", "ostrich", "otter", "owl", "panda", "panther", "parakeet", "parrot", "peacock", "pelican", "penguin", "pheasant", "pig", "pigeon", "piranha", "platypus", "pony", "prawn", "puma", "python", "rabbit", "raccoon", "rat", "rattlesnake", "reindeer", "reptile", "rhinoceros", "roadrunner", "rooster", "roundworm", "sailfish", "salamander", "salmon", "sawfish", "scorpion", "seahorse", "shark", "sheep", "shrimp", "silkworm", "silverfish", "snail", "snake", "snipe", "sole", "sparrow", "spider", "spoonbill", "squid", "squirrel", "starfish", "stingray", "stoat", "sturgeon", "swan", "swordfish", "tapir", "tarantula", "termite", "tern", "tiger", "toad", "toucan", "trout", "tuna", "turkey", "turtle", "viper", "vole", "walrus", "wasp", "weasel", "whale", "whitefish", "wildcat", "wildebeest", "wolf", "wolverine", "wombat", "woodpecker", "worm", "yak", "zebra", "alpaca", "cat", "chicken", "dog", "donkey", "ferret", "goldfish", "guppy", "horse", "koi", "llama", "sheep", "yak"];
+
+function pick(a) {
+  return a[Math.floor(Math.random() * a.length)];
+}
+
+$("#room").attr('placeholder', pick(colors) + "-" + pick(animals));
+
 $("#joinform").submit(function (event) {
-  location.pathname = "/" + $("#room").val().toLowerCase();
+  let room = $("#room").val().toLowerCase() || $("#room").attr('placeholder');
+  location.pathname = "/" + room;
   event.preventDefault();
 });
